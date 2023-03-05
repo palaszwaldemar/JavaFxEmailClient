58c4a2ae5dca75d15eb121260a27030c38a7194c
palaszwaldemar <palasz.waldemar0@gmail.com>
2023-03-05 13:54:32 +0100

    show login window

--- src/email/Launcher.java
+++ src/email/Launcher.java
@@ -11,8 +11,8 @@ import javafx.stage.Stage;
 public class Launcher extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        Parent parent = FXMLLoader.load(getClass().getResource("view/first.fxml"));
-        Scene scene = new Scene(parent, 300, 300);
+        Parent parent = FXMLLoader.load(getClass().getResource("view/LoginWindow.fxml"));
+        Scene scene = new Scene(parent, 458, 316);
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/email/view/LoginWindowController.java
@@ -0,0 +1,22 @@
+package email.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginWindowController {
+    @FXML
+    private TextField emailAddressField;
+
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    void loginButtonAction() {
+        System.out.println("click!");
+    }
+}
