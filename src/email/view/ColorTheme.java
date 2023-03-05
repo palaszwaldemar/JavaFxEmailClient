d3e4145adaba57c5c6514f5dfd6cfffe263ba5d4
palaszwaldemar <palasz.waldemar0@gmail.com>
2023-03-05 23:00:32 +0100

    ability to change the font size and theme

--- src/email/view/ColorTheme.java
+++ src/email/view/ColorTheme.java
@@ -3,5 +3,13 @@ package email.view;
 public enum ColorTheme {
     LIGHT,
     DEFAULT,
-    DARK
+    DARK;
+
+    public static String getCssPath(ColorTheme colorTheme) {
+        return switch (colorTheme) {
+            case LIGHT -> "css/themeLight.css";
+            case DARK -> "css/themeDark.css";
+            case DEFAULT -> "css/themeDefault.css";
+        };
+    }
 }
--- src/email/view/FontSize.java
+++ src/email/view/FontSize.java
@@ -3,5 +3,13 @@ package email.view;
 public enum FontSize {
     SMALL,
     MEDIUM,
-    BIG
+    BIG;
+
+    public static String getCssPath(FontSize fontSize) {
+        return switch (fontSize) {
+            case BIG -> "css/fontBig.css";
+            case SMALL -> "css/fontSmall.css";
+            case MEDIUM -> "css/fontMedium.css";
+        };
+    }
 }
